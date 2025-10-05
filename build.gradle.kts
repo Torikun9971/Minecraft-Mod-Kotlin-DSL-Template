efb982dd84ed2e02ce0938ba1a4b22d8a20a371c
Torikun9971 <msn20220326@gmail.com>
2025-10-06 00:49:54 +0900

    :art: Unify access to gradle.properties and rename property() to prop()

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,20 +8,20 @@ plugins {
 
 val mcVersion = libs.versions.minecraft.get()
 
-version = "${property("mod_version")}+$mcVersion-${property("mod_loader").lowercase()}"
-group = property("mod_group_id")
+version = "${prop("mod_version")}+$mcVersion-${prop("mod_loader").lowercase()}"
+group = prop("mod_group_id")
 
 repositories {
     mavenLocal()
 }
 
 base {
-    archivesName = "${properties["mod_file_name"]}"
+    archivesName = prop("mod_file_name")
 }
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(property("java_version"))
+        languageVersion = JavaLanguageVersion.of(prop("java_version"))
     }
 }
 
@@ -39,8 +39,8 @@ minecraft {
 
             if (canSpecifyUser()) {
                 arguments(
-                    "--username", property("mc_username"),
-                    "--uuid", property("mc_uuid")
+                    "--username", prop("mc_username"),
+                    "--uuid", prop("mc_uuid")
                 )
             }
 
@@ -48,21 +48,21 @@ minecraft {
         }
 
         create("client") {
-            systemProperty("forge.enabledGameTestNamespaces", property("mod_id"))
+            systemProperty("forge.enabledGameTestNamespaces", prop("mod_id"))
         }
 
         create("server") {
-            systemProperty("forge.enabledGameTestNamespaces", property("mod_id"))
+            systemProperty("forge.enabledGameTestNamespaces", prop("mod_id"))
             argument("--nogui")
         }
 
         create("gameTestServer") {
-            systemProperty("forge.enabledGameTestNamespaces", property("mod_id"))
+            systemProperty("forge.enabledGameTestNamespaces", prop("mod_id"))
         }
 
         create("data") {
             arguments(
-                "--mod", property("mod_id"),
+                "--mod", prop("mod_id"),
                 "--all",
                 "--output", file("src/generated/resources/").absolutePath,
                 "--existing", file("src/main/resources/").absolutePath
@@ -96,21 +96,21 @@ tasks.named<Wrapper>("wrapper").configure {
 tasks.named<ProcessResources>("processResources") {
     val replaceProperties = mapOf(
             "minecraft_version" to mcVersion,
-            "minecraft_version_range" to properties["minecraft_version_range"],
+            "minecraft_version_range" to prop("minecraft_version_range"),
             "neoforge_version" to libs.versions.neoforge.get(),
-            "neoforge_version_range" to properties["neoforge_version_range"],
-            "loader_version_range" to properties["loader_version_range"],
-            "mod_id" to properties["mod_id"],
-            "mod_name" to properties["mod_name"],
-            "mod_version" to properties["mod_version"],
-            "mod_license" to properties["mod_license"],
-//            "mod_issue" to properties["mod_issue"],
-//            "mod_update_json" to properties["mod_update_json"],
-//            "mod_homepage" to properties["mod_homepage"],
-//            "mod_icon" to properties["mod_icon"],
-//            "mod_credits" to properties["mod_credits"],
-            "mod_authors" to properties["mod_authors"],
-            "mod_description" to properties["mod_description"],
+            "neoforge_version_range" to prop("neoforge_version_range"),
+            "loader_version_range" to prop("loader_version_range"),
+            "mod_id" to prop("mod_id"),
+            "mod_name" to prop("mod_name"),
+            "mod_version" to prop("mod_version"),
+            "mod_license" to prop("mod_license"),
+//            "mod_issue" to prop("mod_issue"),
+//            "mod_update_json" to prop("mod_update_json"),
+//            "mod_homepage" to prop("mod_homepage"),
+//            "mod_icon" to prop("mod_icon"),
+//            "mod_credits" to prop("mod_credits"),
+            "mod_authors" to prop("mod_authors"),
+            "mod_description" to prop("mod_description"),
     )
 
     inputs.properties(replaceProperties)
@@ -144,7 +144,7 @@ idea {
     }
 }
 
-fun property(key: String): String {
+fun prop(key: String): String {
     return properties[key].toString()
 }
 
