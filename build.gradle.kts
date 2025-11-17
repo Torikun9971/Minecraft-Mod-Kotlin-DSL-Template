d395d31525739661c9041327da881d490a737be5
Torikun9971 <msn20220326@gmail.com>
2025-11-18 04:54:31 +0900

    :sparkles: Add support for releasing to Modrinth and CurseForge (commented out by default)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import net.darkhax.curseforgegradle.TaskPublishCurseForge
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -8,6 +9,9 @@ plugins {
     alias(libs.plugins.forgegradle)
     alias(libs.plugins.parchment.forgegradle)
     alias(libs.plugins.mixin)
+
+    alias(libs.plugins.curseforgegradle)
+    alias(libs.plugins.minotaur)
 }
 
 val mcVersion = libs.versions.minecraft.get()
@@ -156,6 +160,9 @@ tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
 }
 
+val releaseTitle = "${prop("mod_name")} For ${prop("mod_loader")} $mcVersion"
+//val changelogFile = file("/CHANGELOG.md")
+
 tasks.register("printReleaseVersion") {
     println(version)
 }
@@ -173,8 +180,65 @@ publishing {
     }
 }
 
+/**
+tasks.register<TaskPublishCurseForge>("curseforge") {
+    apiToken = System.getenv("CURSEFORGE_TOKEN")
+
+    upload(prop("curseforge_id"), tasks.jar) {
+        displayName = releaseTitle
+
+        addEnvironment(prop_list("curseforge_environments"))
+        addModLoader(prop_list("release_loaders"))
+        addJavaVersion(prop_list("curseforge_java_versions"))
+        addGameVersion(prop_list("release_minecraft_versions"))
+
+        withAdditionalFile(sourcesJar())
+        releaseType = prop("release_type")
+
+        /**
+        if (changelogFile.exists()) {
+            changelog = changelogFile
+            changelogType = "markdown"
+        }
+        **/
+    }
+}
+**/
+
+/**
+modrinth {
+    token = System.getenv("MODRINTH_TOKEN")
+
+    projectId = prop("modrinth_id")
+    uploadFile.set(tasks.jar)
+
+    versionName = releaseTitle
+
+    /**
+    if (changelogFile.exists()) {
+        changelog.set(changelogFile.readText())
+    }
+    **/
+
+    versionType.set(prop("release_type"))
+    versionNumber.set(version.toString())
+    loaders.addAll(prop_list("release_loaders").map { it.lowercase() })
+    gameVersions.addAll(prop_list("release_minecraft_versions"))
+
+    dependencies {
+
+    }
+
+    additionalFiles.add(sourcesJar())
+}
+**/
+
 fun prop(key: String): String {
     return properties[key].toString()
+}
+
+fun prop_list(key: String): List<String> {
+    return properties[key].toString().split(",")
 }
 
 fun extra(key: String): String {
@@ -187,4 +251,8 @@ fun Provider<MinimalExternalModuleDependency>.classifier(name: String): String {
 
 fun canSpecifyUser(): Boolean {
     return hasProperty("mc_username") && hasProperty("mc_uuid")
+}
+
+fun sourcesJar(): File {
+    return tasks.named("sourcesJar").get().outputs.files.singleFile
 }
