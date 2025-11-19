87f3153d55b9e038235a37082d6c59ca3b13d540
Torikun9971 <msn20220326@gmail.com>
2025-11-19 09:07:43 +0900

    :technologist: Update LOGGER name and sample logs to match mod name

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,17 +1,19 @@
 package com.example.examplemod;
 
-import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_NAME = "Example Mod";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     public ExampleMod(IEventBus modEventBus, ModContainer modContainer) {
-        LOGGER.info("[Example Mod] has been loaded!");
+        LOGGER.info("has been loaded!");
     }
 }
