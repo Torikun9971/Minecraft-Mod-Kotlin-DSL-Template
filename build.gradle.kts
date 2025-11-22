e2723a50109ed75788f089711035d2a6ec952487
Torikun9971 <msn20220326@gmail.com>
2025-11-22 14:56:22 +0900

    :wrench: Disable automatic version detection in CurseForge release task

--- build.gradle.kts
+++ build.gradle.kts
@@ -162,6 +162,8 @@ publishing {
 tasks.register<TaskPublishCurseForge>("curseforge") {
     apiToken = System.getenv("CURSEFORGE_TOKEN")
 
+    disableVersionDetection()
+
     upload(prop("curseforge_id"), tasks.jar) {
         displayName = releaseTitle
 
