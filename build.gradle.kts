0b7dfc38307b01e5b0efafb0e6451b64f605894d
Torikun9971 <msn20220326@gmail.com>
2025-12-04 04:20:37 +0900

    :art: Improve release title formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -138,7 +138,7 @@ idea {
     }
 }
 
-val releaseTitle = "${prop("mod_name")} For ${prop("mod_loader")} $mcVersion"
+val releaseTitle = "${prop("mod_name")} ${prop("mod_version")} for ${prop("mod_loader")} $mcVersion"
 //val changelogFile = file("changelog.md")
 
 tasks.register("printReleaseVersion") {
