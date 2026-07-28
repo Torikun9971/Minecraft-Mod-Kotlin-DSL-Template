bf8772038ea80097fa4c4b4e3d13f9b0a68b7397
Torikun9971 <msn20220326@gmail.com>
2026-07-28 09:38:33 +0900

    :truck: プロジェクト名をリネーム

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,8 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
+val parent = rootDir.parentFile.name
+val root = rootDir.name
+
+rootProject.name = "${parent}+${root}"
